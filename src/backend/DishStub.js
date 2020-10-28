89eeb17779faff954f6d1019ee2d6d3b1547809a
Devin Efendy <devinefendy.dev@gmail.com>
2020-10-27 21:13:03 -0500

    Reconfigure project folder structure

---
+++ src/backend/DishStub.js
@@ -0,0 +1,12 @@
+export const DishStub = [
+    {
+        dishName: "restaurant-name",
+        dishPhoto: "../asset/dish/photo.jpg",
+        description: "the food is good af",
+        review: 5.0,
+        price: 15.0
+    },
+    {
+
+    }
+]
---
+++ src/backend/RestaurantStub.js
@@ -0,0 +1,12 @@
+export const RestaurantStub = [
+    {
+        restaurantName: "restaurant-name",
+        restaurantLogo: "../asset/restaurant/logo.jpg",
+        deliveryTime: "hh hours mm minutes",
+        deliveryFee: 5.0,
+        review: 5
+    },
+    {
+
+    }
+]
