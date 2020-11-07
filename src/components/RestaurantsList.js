c7469dd68341c38d27230a23af59120a174112e7
raul-gonzales <68661605+raul-gonzales@users.noreply.github.com>
2020-11-07 16:01:16 -0600

    Added array for restaurant info
    Cleaned up the code a bit, (still trying to figure out how to use restaurantStub array into this function)

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -2,52 +2,51 @@ import React from 'react'
 import '../css/RestaurantsList.css'
 
 function RestaurantsList() {
-    return (
+    //Information for restaurants
+    const restaurantInfo =[
+        {
+        restaurant_name: 'McDonalds',
+        restaurant_rating: 'O O O O X',
+        restaurant_logo: '*Insert*LOGO*',
+        delivery_time: 30,
+        delivery_fee: 1.00
+        },
+        {
+        restaurant_name: 'Burger King',
+        restaurant_rating: 'O O O X X',
+        restaurant_logo: '*Insert*LOGO*',
+        delivery_time: 25,
+        delivery_fee: 0.50
+        },
+        {
+        restaurant_name: 'Pizza Hut',
+        restaurant_rating: 'O O O O O',
+        restaurant_logo: '*Insert*LOGO*',
+        delivery_time: 20,
+        delivery_fee: 0.30
+        }
+    ]
+
+    //formatting the restaurant boxes/info
+    const restaurantList = restaurantInfo.map(theInfo=> (
         <div className="restaurants">
-            <div className="restaurantContainer">
-                <h2>Restaurant near you #1</h2>
-            <p className="ratings">Ratings: O O O O X</p>
-            <p className="dummyLogo">**Dummy Logo**</p>
-            <button>Select</button>
-            <ul>
-                <li>Delivery time: 30 mins.</li>
-                <li>Delivery fee: $0.00</li>
-            </ul>
+                <div className="restaurantContainer">
+                <h2>{theInfo.restaurant_name}</h2>
+                <p className="ratings">Ratings: {theInfo.restaurant_rating}</p>
+                <p className="dummyLogo">{theInfo.restaurant_logo}</p>
+                <button>Select</button>
+                <ul>
+                    <li>Delivery time: {theInfo.delivery_time} mins.</li>
+                    <li>Delivery fee: ${theInfo.delivery_fee}</li>
+                </ul>
             </div>
-
-            <div className="restaurantContainer">
-            <h2>Restaurant near you #2</h2>
-            <p className="ratings">Ratings: O O O X X</p>
-            <p className="dummyLogo">**Dummy Logo**</p>
-            <button>Select</button>
-            <ul>
-                <li>Delivery time: 20 mins.</li>
-                <li>Delivery fee: $0.00</li>
-            </ul>
-            </div>
-
-            <div className="restaurantContainer">
-            <h2>Restaurant near you #3</h2>
-            <p className="ratings">Ratings: O O X X X</p>
-            <p className="dummyLogo">**Dummy Logo**</p>
-            <button>Select</button>
-            <ul>
-                <li>Delivery time: 15 mins.</li>
-                <li>Delivery fee: $0.00</li>
-            </ul>
-            </div>
-
-            <div className="restaurantContainer">
-            <h2>Restaurant near you #4</h2>
-            <p className="ratings">Ratings: O O O O O</p>
-            <p className="dummyLogo">**Dummy Logo**</p>
-            <button>Select</button>
-            <ul>
-                <li>Delivery time: 47 mins.</li>
-                <li>Delivery fee: $0.00</li>
-            </ul>
         </div>
-    </div>
+    ))
+
+    return (
+        <div>
+            {restaurantList}
+        </div>
     )
 }
 
