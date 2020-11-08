0294a7d04b9dc50e6eb06275c8af5d6ebc7f831f
raul-gonzales <68661605+raul-gonzales@users.noreply.github.com>
2020-11-07 18:52:01 -0600

    RestaurantList updated
    -added font awesome in index.html
    -"export default" on restaurant stub (allowed me to use the array in the stub)
    -RestauranList now uses info from the restaurant stub

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,43 +1,21 @@
 import React from 'react'
 import '../css/RestaurantsList.css'
+import RestaurantStub from '../backend/RestaurantStub';
+
 
 function RestaurantsList() {
-    //Information for restaurants
-    const restaurantInfo =[
-        {
-        restaurant_name: 'McDonalds',
-        restaurant_rating: 'O O O O X',
-        restaurant_logo: '*Insert*LOGO*',
-        delivery_time: 30,
-        delivery_fee: 1.00
-        },
-        {
-        restaurant_name: 'Burger King',
-        restaurant_rating: 'O O O X X',
-        restaurant_logo: '*Insert*LOGO*',
-        delivery_time: 25,
-        delivery_fee: 0.50
-        },
-        {
-        restaurant_name: 'Pizza Hut',
-        restaurant_rating: 'O O O O O',
-        restaurant_logo: '*Insert*LOGO*',
-        delivery_time: 20,
-        delivery_fee: 0.30
-        }
-    ]
-
     //formatting the restaurant boxes/info
-    const restaurantList = restaurantInfo.map(theInfo=> (
+    const restaurantList = RestaurantStub.map(theInfo=> (
         <div className="restaurants">
                 <div className="restaurantContainer">
-                <h2>{theInfo.restaurant_name}</h2>
-                <p className="ratings">Ratings: {theInfo.restaurant_rating}</p>
-                <p className="dummyLogo">{theInfo.restaurant_logo}</p>
+                <h2>{theInfo.restaurantName}</h2>
+                <p className="ratings">Ratings: {theInfo.review} stars</p>
+                <p className="restaurantLogo"><i class={theInfo.restaurantLogo}></i></p>
+                
                 <button>Select</button>
                 <ul>
-                    <li>Delivery time: {theInfo.delivery_time} mins.</li>
-                    <li>Delivery fee: ${theInfo.delivery_fee}</li>
+                    <li>Delivery time: {theInfo.deliveryTime}</li>
+                    <li>Delivery fee: ${theInfo.deliveryFee}</li>
                 </ul>
             </div>
         </div>
